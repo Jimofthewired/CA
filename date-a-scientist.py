5ec66bf7754b1fc879f36da58dc09d3a045e135f
Jimofthewired <jamesgodsell@blueyonder.co.uk>
2024-05-02 22:57:20 +0100

    first commit

---
+++ date-a-scientist.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
+
+
+# In[ ]:
+
+
+
+
